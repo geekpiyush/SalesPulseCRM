ef31857fb4e7af2a7bd01b1ac0a170d78528691e
Piyush <dpiyush28@gmail.com>
2026-03-23 19:20:06 +0530

    fix: correct deployment path

---
+++ SalesPulseCRM.Application/DTOs/CreateLeadDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesPulseCRM.Application.DTOs
+{
+   
+        public class CreateLeadDto
+        {
+
+        [Required(ErrorMessage ="Customer Name Can't be Blank")] 
+            public string CustomerName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Phone Number Can't Blank")]
+             public string Phone { get; set; } = null!;
+
+        [Required(ErrorMessage = "Email Number Can't Blank")]
+             public string? Email { get; set; }
+
+             public string LeadSource { get; set; } = "Meta";
+
+            public string LeadStatus { get; set; } = "New"; // default
+
+            //public int? AssignedTo { get; set; }
+        }
+    
+}
---
+++ SalesPulseCRM.WEB/Controllers/LeadController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesPulseCRM.Application.DTOs;
+using SalesPulseCRM.Domain.Entities;
+using SalesPulseCRM.Infrastructure.DB;
+using Xunit.Sdk;
+
+namespace SalesPulseCRM.WEB.Controllers
+{
+    public class LeadController : Controller
+    {
+        private readonly CrmDbContext _db;
+        public LeadController(CrmDbContext crmDbContext)
+        {
+            _db = crmDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult CreateLead()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLead(CreateLeadDto createLeadDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(createLeadDto);
+            }
+
+            //if(createLeadDto == null)
+            //{
+
+            //}
+
+            var existingLead = await _db.Leads.FirstOrDefaultAsync(temp => temp.Phone == createLeadDto.Phone);
+
+            if (existingLead != null)
+            {
+                ModelState.AddModelError("Phone", "Phone Number Already Exist");
+
+                return View(createLeadDto);
+            }
+
+            var leads = new Lead { CustomerName = createLeadDto.CustomerName, Email = createLeadDto.Email, Phone = createLeadDto.Phone, LeadStatus = createLeadDto.LeadStatus };
+                
+            _db.Leads.Add(leads);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+
+            
+        }
+
+    }
+}
